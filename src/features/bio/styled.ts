6959b85510656f45c9483c2955102663dfe3e2a8
andresfelipefajardoparra <93097366+andresfelipefajardoparra@users.noreply.github.com>
2023-04-01 17:52:27 -0500

    ahora si funciona

--- src/features/bio/styled.ts
+++ src/features/bio/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 
  export const ContenedorBotones = styled.div`
-  background-color: whitesmoke;
+  background-color: rgb(255, 0, 255);
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -11,26 +11,5 @@ import styled from "styled-components"
   margin-bottom: 1rem;
   cursor: progress;
   `;
-
-
-export const BotonBioActivo = styled.button`
-
-border-radius: 5px;
-  border: 1px solid darkgray;
-  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
-  padding: 1rem;
-  margin: 1rem;
-  font-family: "Homer Simpson Revised", sans-serif;
-  font-size: 1.4rem;
-  background-color: red;
-  color: whitesmoke;
-  text-shadow: 2px 2px 0 #000000, 2px -2px 0 #000000, -2px 2px 0 #000000,
-    -2px -2px 0 #000000, 2px 0px 0 #000000, 0px 2px 0 #000000,
-    -2px 0px 0 #000000, 0px -2px 0 #000000;
-    cursor: wait;
-
-    
-`
-
 
    
