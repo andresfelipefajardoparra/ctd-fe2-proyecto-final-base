93aea9dec9f8dff4945e924dfcd3ff1a7997eaee
andresfelipefajardoparra <93097366+andresfelipefajardoparra@users.noreply.github.com>
2023-04-03 19:29:21 -0500

    estilo prueba

--- src/features/bio/styled.ts
+++ src/features/bio/styled.ts
@@ -4,13 +4,14 @@ import { ContenedorCita } from "../quote/styled";
 
 
  export const ContenedorBotones = styled.div`
-  background-color: rgb(255, 0, 255);
+  background-color: #AED6F1;
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   margin-bottom: 1rem;
+  cursor: not-allowed;
   
   `;
 
@@ -23,13 +24,13 @@ import { ContenedorCita } from "../quote/styled";
         border-radius: 5px;
         border: 1px solid darkgray;
         box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
-        padding: 1rem;
-        margin: 1rem;
+        padding: 2rem;
+        margin: 2rem;
         font-family: "Homer Simpson Revised", sans-serif;
         font-size: 1.4rem;
         background-color: ${({isActive}) => isActive? "red" : "#fdd835"};
         color: ${({isActive}) => isActive? "blue" : "whitesmoke"};
-         text-shadow: ${({isActive}) => isActive? "2px 2px 0 #000000, 2px -2px 0 #000000, -2px 2px 0 #000000,-2px -2px 0 #000000, 2px 0px 0 #000000, 0px 2px 0 #000000,-2px 0px 0 #000000, 0px -2px 0 #000000" : "10px"};
+         text-shadow: ${({isActive}) => isActive? "2px 2px 0 #000000, 2px -2px 0 #000000, -2px 2px 0 #000000,-2px -2px 0 #000000, 2px 0px 0 #000000, 0px 2px 0 #000000,-2px 0px 0 #000000, 0px -2px 0 #000000" : ""};
          cursor: progress;
         
          ${props => props.isActive && css`
